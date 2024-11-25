271a0768938c4d5c0978cae11b87247a399b35dd
nicoaranc <nicolas.arancibia.aguirre@gmail.com>
2024-11-25 10:09:54 -0300

    Changes in scene and script

--- scripts/player_scripts/slots/slot_special_attack.gd
+++ scripts/player_scripts/slots/slot_special_attack.gd
@@ -1,8 +1,14 @@
 extends Node3D
 
 @onready var machine: RigidBody3D = $Machine
+var INIT_POS = Vector3(0,-2,-2)
 
 func _input(event: InputEvent) -> void:
-	
 	if Input.is_action_just_pressed("special_attack"):
+		machine.show()
 		machine.gravity_scale = 1
+		
+
+
+func _on_machine_body_entered(body: Node) -> void:
+	machine.position = INIT_POS
