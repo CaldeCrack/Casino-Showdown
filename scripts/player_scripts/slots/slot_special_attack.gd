e0928dc84ade8c2f4fdc836c4708e20781ae80bc
nicoaranc <nicolas.arancibia.aguirre@gmail.com>
2024-12-11 23:35:41 -0300

    Add timers to Special and Ultimate Attack

--- scripts/player_scripts/slots/slot_special_attack.gd
+++ scripts/player_scripts/slots/slot_special_attack.gd
@@ -1,6 +1,8 @@
 extends Node3D
 
 var OWNER = get_parent()
+@onready var cooldown: Timer = $Cooldown
+var AVAILABLE: bool = true
 
 var POS1 = Vector3(0,3,-4) + OWNER.position
 var POS2 = Vector3(0,3,4) + OWNER.position
@@ -16,8 +18,10 @@ var slot: PackedScene = preload("res://scenes/players/slot_machine.tscn")
 
 func _input(event: InputEvent) -> void:
 	if is_multiplayer_authority():
-		if Input.is_action_just_pressed("special_attack"):
+		if Input.is_action_just_pressed("special_attack") and AVAILABLE:
 			_inst_slot.rpc()
+			AVAILABLE = false
+			cooldown.start()
 
 @rpc("any_peer", "call_local")
 func _inst_slot() -> void:
@@ -26,3 +30,7 @@ func _inst_slot() -> void:
 		OWNER.get_parent().add_child(machine)
 		machine.global_position = OWNER.global_position + pos
 		machine.collision_shape_3d.scale *= 2
+
+
+func _on_cooldown_timeout() -> void:
+	AVAILABLE = true
