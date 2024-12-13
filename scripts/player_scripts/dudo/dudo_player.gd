e20dcd1f1ea77491c04d27138a4b9263465f52a1
chunchometalero <juan.valdivia.g@ug.uchile.cl>
2024-12-12 21:23:47 -0300

    Cooldowns

--- scripts/player_scripts/dudo/dudo_player.gd
+++ scripts/player_scripts/dudo/dudo_player.gd
@@ -9,11 +9,15 @@ extends Player
 @onready var marker_3d_ultimate_4: Marker3D = $Ultimate/Marker3DUltimate4
 @onready var marker_3d_ultimate_5: Marker3D = $Ultimate/Marker3DUltimate5
 
+@onready var dice_recharge: Timer = $dice_recharge
+@onready var cd_ultimate: Timer = $cd_ultimate
+
+
 const GRAVITY: float = 9.8
 const REBOUND_FACTOR: float = 0.6  # Qué tanto rebota el dado (0 a 1)
 
-# Lista de todos los Marker3D de la habilidad ultimate
-
+var dice_count: int = 5
+var available_ult = 1
 
 func _input(event: InputEvent) -> void:
 	if is_multiplayer_authority():
@@ -21,14 +25,19 @@ func _input(event: InputEvent) -> void:
 		
 		if Input.is_action_just_pressed("special_attack"):
 			_special_attack.rpc()
+			dice_recharge.start()
+			print("Apretaste la e:", dice_count)
 		
-		if Input.is_action_just_pressed("ultimate"):
-			_ultimate_attack.rpc()
+		if available_ult:
+			if Input.is_action_just_pressed("ultimate"):
+				available_ult = 0
+				_ultimate_attack.rpc()
+				cd_ultimate.start()
 
 func _process(delta: float) -> void:
 	if marker_3d and is_instance_valid(marker_3d):
 		marker_3d.rotation = rotation
-		
+
 
 func init_marker() -> void:
 	marker_3d = $SpecialAttack/Marker3D
@@ -43,22 +52,26 @@ func _special_attack() -> void:
 		print("Marker3D no es válido")
 		return
 
-	var dice_instance = DICE.instantiate()  # Instancia el dado
-	if not dice_instance:
-		print("No se pudo instanciar el dado")
-		return
+	if dice_count > 0:
+		var dice_instance = DICE.instantiate()  # Instancia el dado
+		if not dice_instance:
+			print("No se pudo instanciar el dado")
+			return
 
-	get_parent().add_child(dice_instance)  # Añade el dado a la escena
-	dice_instance.global_transform = marker_3d.global_transform  # Coloca el dado en la posición del Marker3D
-
-	# Calcula la dirección inicial
-	var initial_direction = -marker_3d.transform.basis.z.normalized()
-	var initial_velocity = initial_direction * 20.0  # Velocidad inicial (ajusta el valor según lo que quieras)
-
-	# Pasa la velocidad inicial al dado
-	if dice_instance.has_method("launch"):
-		dice_instance.launch(initial_velocity)
+		get_parent().add_child(dice_instance)  # Añade el dado a la escena
+		dice_instance.global_transform = marker_3d.global_transform  # Coloca el dado en la posición del Marker3D
 		
+		# Disminuimos el contador
+		if dice_count > 0:
+			dice_count -= 1
+
+		# Calcula la dirección inicial
+		var initial_direction = -marker_3d.transform.basis.z.normalized()
+		var initial_velocity = initial_direction * 20.0  # Velocidad inicial (ajusta el valor según lo que quieras)
+
+		# Pasa la velocidad inicial al dado
+		if dice_instance.has_method("launch"):
+			dice_instance.launch(initial_velocity)
 
 @rpc("any_peer", "call_local")
 func _ultimate_attack() -> void:
@@ -93,3 +106,15 @@ func _ultimate_attack() -> void:
 		# Lanza el dado con la velocidad calculada
 		if dice_instance.has_method("launch"):
 			dice_instance.launch(initial_velocity)
+
+
+func _on_dice_recharge_timeout() -> void:
+	if dice_count < 5:
+		dice_count += 1
+		print("un dado adicional")
+		print(dice_count)
+
+
+func _on_cd_ultimate_timeout() -> void:
+	print("Ulti disponible")
+	available_ult = 1
